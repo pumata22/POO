e878d7fbcc158c8d9854e88fad3db79ca1eccf72
Mateus <pumata1928@gmail.com>
2024-10-02 18:37:49 +0100

    Todas as aulas ate agora

---
+++ Aulas/Aula 1 - Basico/Aula 1/Aula 1/Program.cs
@@ -0,0 +1,55 @@
+﻿//-----------------------------------------------------------------
+//    <copyright file="Aula_1.cs"    company="IPCA">
+//     Copyright (c) IPCA-EST 2024. All rights reserved.
+//    </copyright>
+//    <date>2024-09-26</date>
+//    <time>16:01</time>
+//    <version>0.1</version>
+//    <author>tcastro</author>
+//    <description></description>
+//-----------------------------------------------------------------
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1___Basico
+{
+    /*
+      * LEIM: POO
+      * tcastro
+      * 
+      * Soluções e Projetos
+      * Wizards
+      * Regiões
+      * Documentação de Código
+    * */
+    internal class Program
+    {
+        #region ZonaDeTeste 
+        static void Main(string[] args)
+        {
+            //Criar Soluções e Projetos
+            Console.WriteLine("Hello world!");
+
+            double CT, CP, NF;
+
+            Console.Write("\nInsira a nota de Componente Teórica (CT): ");
+            CT = double.Parse(Console.ReadLine());
+
+            Console.Write("\nInsira a nota de Componente Prática (CP): ");
+            CP = double.Parse(Console.ReadLine());
+
+            // Calcular a média final (NF) com ponderações: 40% CT e 60% CP
+            NF = (CT * 0.4) + (CP * 0.6);
+
+            Console.WriteLine("\nA nota de CT é {0:F2} e de CP {1:F2}, dando uma média final da UC de POO igual a {2:F2}", CT, CP, NF);
+
+            Console.ReadKey();
+        }
+        #endregion
+    }
+}
---
+++ Aulas/Aula 3 - Fundamentos/Aula 3 - Fundamentos/media.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media_C_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            float tc, pc, codeGrade, oralGrade, media1;
+
+            Console.WriteLine("Calculate your media!");
+            Console.WriteLine("Insert your Theorical Component(TC) grade: ");
+            tc = float.Parse(Console.ReadLine());
+            Console.WriteLine("Insert your code grade: ");
+            codeGrade = float.Parse(Console.ReadLine());
+            Console.WriteLine("Insert you oral grade: ");
+            oralGrade = float.Parse(Console.ReadLine());
+
+            pc = (float)(codeGrade * (0.5)) + (float)(oralGrade * (0.5));
+
+            media1 = (float)(0.4 * tc) + (float)(0.6 * pc);
+            //Math.Round(average,2) arredonda com 2 casas decimais
+
+            Console.WriteLine("Your media is: " + media1);
+
+        }
+    }
+}
